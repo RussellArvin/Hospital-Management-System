cb3f94ee05a3339020e791cf0f12607f4c40a78b
mommypokko <c3l3st7@gmail.com>
2024-11-15 02:52:54 +0800

    chore: added javadoc for Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,16 @@
 package controller;
 
+/**
+ * Interface that defines the contract for controllers in the application.
+ * Implementing classes must provide a method to handle user input.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0
+ */
 interface Controller {
+    
+    /**
+     * Method to handle user input. Implementing classes must define the specific behavior.
+     */
     void handleUserInput();
 }
