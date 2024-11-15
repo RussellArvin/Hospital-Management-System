6a6b3e4e24390585c4d2dc18b04360d4a21f1bb4
mommypokko <c3l3st7@gmail.com>
2024-11-15 12:49:54 +0800

    chore: added javadoc for Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,24 @@
 package main;
 
+import controller.MainController;
 import java.util.Scanner;
 
-import controller.MainController;
-
+/**
+ * The Main class serves as the entry point for the application.
+ * It initializes necessary components and begins handling user input.
+ * 
+ * @author Celeste Ho 
+ * @version 1.0
+ */
 public class Main {
+
+    /**
+     * The main method is the starting point of the application.
+     * It creates a Scanner object for user input, initializes the
+     * MainController, and begins processing user interactions.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
