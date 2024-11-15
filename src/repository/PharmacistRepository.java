d860bea977b968932c4b7d85695f49759cc9f9b4
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:52:27 +0800

    chore: added javadoc for PharmacistRepository

--- src/repository/PharmacistRepository.java
+++ src/repository/PharmacistRepository.java
@@ -4,14 +4,32 @@ import model.Pharmacist;
 import repository.base.CsvRepository;
 import repository.mapper.PharmacistMapper;
 
+/**
+ * The PharmacistRepository class extends CsvRepository for managing Pharmacist entities.
+ * It specifies the CSV file and header used for Pharmacist data and provides methods
+ * for accessing and managing Pharmacist records.
+ * 
+ * @author Tan Jou Yuan
+ * @version 1.0
+ */
 public class PharmacistRepository extends CsvRepository<Pharmacist, PharmacistMapper> {
+
     private static final String CSV_FILE = "data/pharmacists.csv";
     private static final String CSV_HEADER = "id,password,salt,name,age,gender,createdAt,updatedAt";
 
+    /**
+     * Constructs a PharmacistRepository with predefined CSV file and header information
+     * for storing Pharmacist data and initializes the PharmacistMapper.
+     */
     public PharmacistRepository() {
         super(CSV_FILE, CSV_HEADER, new PharmacistMapper());
     }
 
+    /**
+     * Retrieves all Pharmacist records from the CSV file.
+     *
+     * @return An array of all Pharmacist objects in the repository.
+     */
     @Override
     public Pharmacist[] findAll() {
         return super.findAll(Pharmacist.class);
