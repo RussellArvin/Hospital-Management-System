b1f2c926842ba7af624ed5e3746d32f889917a04
mommypokko <c3l3st7@gmail.com>
2024-11-15 20:02:41 +0800

    chore: added javadoc for NurseMenuUI

--- src/ui/NurseMenuUI.java
+++ src/ui/NurseMenuUI.java
@@ -1,6 +1,18 @@
 package ui;
 
+/**
+ * The NurseMenuUI class provides the user interface for the nurse's menu.
+ * It extends the MenuUI class and displays the available options specific to nurses.
+ * 
+ * @author Tan Jou Yuan
+ * @version 1.0
+ */
 public class NurseMenuUI extends MenuUI {
+
+    /**
+     * Prints the options available in the nurse menu.
+     * These options include managing patients, viewing appointments, recording patient vitals, and logging out.
+     */
     public void printOptions() {
         printMenu("Nurse Menu",
             "1. View and Manage Patients",
