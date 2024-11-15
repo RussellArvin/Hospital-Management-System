61dbabc9175b3fe0fadc23a154a51bf3de7d03ab
mommypokko <c3l3st7@gmail.com>
2024-11-15 20:08:31 +0800

    chore: added javadoc for PharmacistMenuUI

--- src/ui/PharmacistMenuUI.java
+++ src/ui/PharmacistMenuUI.java
@@ -1,6 +1,19 @@
 package ui;
 
+/**
+ * The PharmacistMenuUI class provides the user interface for the pharmacist's menu.
+ * It extends the MenuUI class and displays the available options specific to pharmacists.
+ * 
+ * @author Lim Jun Howe
+ * @version 1.0
+ */
 public class PharmacistMenuUI extends MenuUI {
+
+    /**
+     * Prints the options available in the pharmacist menu.
+     * These options include viewing appointment outcomes, managing prescription statuses,
+     * checking medication inventory, submitting replenishment requests, and logging out.
+     */
     public void printOptions() {
         printMenu("Pharmacist Menu",
             "1. View Appointment Outcome Record",
