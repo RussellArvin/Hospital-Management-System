6dd7c015146ce3aefa24cf4f8ab8581231336d66
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:28:49 +0800

    chore: added javadoc for AppointmentMapper

--- src/repository/mapper/AppointmentMapper.java
+++ src/repository/mapper/AppointmentMapper.java
@@ -1,15 +1,28 @@
 package repository.mapper;
 
-import java.time.LocalDateTime;
-
 import enums.AppointmentStatus;
+import java.time.LocalDateTime;
 import model.Appointment;
 
+/**
+ * The AppointmentMapper class provides a mapping from a CSV line to an Appointment object.
+ * It implements the BaseMapper interface for the Appointment type.
+ * 
+ * @author Lim Jun Howe
+ * @version 1.0 
+ */
 public class AppointmentMapper implements BaseMapper<Appointment> {
+
+    /**
+     * Converts a CSV line into an Appointment object by parsing each field.
+     *
+     * @param csvLine The CSV line containing Appointment data.
+     * @return An Appointment object constructed from the CSV data.
+     */
     public Appointment fromCsvString(String csvLine) {
         String[] parts = csvLine.split(",");
 
-        return new Appointment (
+        return new Appointment(
             parts[0],
             parts[1],
             parts[2],
