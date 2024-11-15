6fb23f33f3271fdb273e5053be5a593e1fdc8d2e
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:05:55 +0800

    chore: added javadoc for Nurse

--- src/model/Nurse.java
+++ src/model/Nurse.java
@@ -1,10 +1,29 @@
 package model;
 
+import enums.Gender;
 import java.time.LocalDateTime;
 
-import enums.Gender;
-
+/**
+ * The Nurse class represents a nurse in the system, extending the User class.
+ * It includes attributes inherited from User, such as id, password, name, age, gender, and timestamps.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0 
+ */
 public class Nurse extends User {
+
+    /**
+     * Constructs a Nurse with the specified details.
+     *
+     * @param id         The unique ID of the nurse.
+     * @param password   The password of the nurse.
+     * @param salt       The salt used for password hashing.
+     * @param name       The name of the nurse.
+     * @param age        The age of the nurse.
+     * @param gender     The gender of the nurse.
+     * @param createdAt  The date and time when the nurse was created.
+     * @param updatedAt  The date and time when the nurse was last updated.
+     */
     public Nurse(
         String id,
         String password,
@@ -15,7 +34,6 @@ public class Nurse extends User {
         LocalDateTime createdAt,
         LocalDateTime updatedAt
     ) {
-
         super(id, password, salt, name, age, gender, createdAt, updatedAt);
     }
 }
