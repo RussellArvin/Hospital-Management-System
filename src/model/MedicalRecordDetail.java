16741fb92842f496484ce4e1b36127d4cfe91787
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:03:41 +0800

    chore: added javadoc for MedicalRecordDetail

--- src/model/MedicalRecordDetail.java
+++ src/model/MedicalRecordDetail.java
@@ -1,12 +1,31 @@
 package model;
 
+import enums.MedicalRecordType;
 import java.time.LocalDateTime;
 
-import enums.MedicalRecordType;
-
+/**
+ * The MedicalRecordDetail class extends MedicalRecord, providing additional details
+ * about the doctor associated with the medical record.
+ * 
+ * @author Russell Arvin 
+ * @version 1.0 
+ */
 public class MedicalRecordDetail extends MedicalRecord {
+
     private Doctor doctor;
 
+    /**
+     * Constructs a MedicalRecordDetail with the specified details, including the associated doctor.
+     *
+     * @param id         The unique ID of the medical record.
+     * @param patientId  The ID of the patient associated with this medical record.
+     * @param doctorId   The ID of the doctor associated with this medical record.
+     * @param type       The type of the medical record.
+     * @param details    Specific details of the medical record.
+     * @param createdAt  The date and time when the medical record was created.
+     * @param updatedAt  The date and time when the medical record was last updated.
+     * @param doctor     The doctor associated with this medical record.
+     */
     public MedicalRecordDetail(
         String id,
         String patientId,
@@ -21,10 +40,23 @@ public class MedicalRecordDetail extends MedicalRecord {
         this.doctor = doctor;
     }
 
-    public Doctor getDoctor(){
+    /**
+     * Gets the doctor associated with this medical record.
+     *
+     * @return The Doctor associated with this medical record.
+     */
+    public Doctor getDoctor() {
         return this.doctor;
     }
 
+    /**
+     * Creates a MedicalRecordDetail object from an existing MedicalRecord, associating it with
+     * the specified doctor details.
+     *
+     * @param record The existing MedicalRecord object to use as a base.
+     * @param doctor The doctor associated with this medical record.
+     * @return A new MedicalRecordDetail object with doctor details.
+     */
     public static MedicalRecordDetail fromMedicalRecord(
         MedicalRecord record,
         Doctor doctor
