d67d5450a3486f5667104b2ef63dce921828bad7
mommypokko <c3l3st7@gmail.com>
2024-11-15 20:22:11 +0800

    chore: added javadoc for Constant

--- src/util/Constant.java
+++ src/util/Constant.java
@@ -1,7 +1,26 @@
 package util;
 
+/**
+ * The Constant class provides a collection of commonly used constants
+ * in the application, such as default values for passwords and work hours.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0
+ */
 public class Constant {
+
+    /** 
+     * The default password used for initializing user accounts. 
+     */
     public static final String DEFAULT_PASSWORD = "password";
+
+    /** 
+     * The default starting hour of work for staff members (in 24-hour format). 
+     */
     public static final int DEFAULT_START_WORK_HOURS = 9;
+
+    /** 
+     * The default ending hour of work for staff members (in 24-hour format). 
+     */
     public static final int DEFAULT_END_WORK_HOURS = 17;
 }
