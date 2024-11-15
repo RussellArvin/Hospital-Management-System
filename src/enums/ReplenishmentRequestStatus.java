61c13e67af4ab8d24e4cf8d4d6674cc1248c6bd0
mommypokko <c3l3st7@gmail.com>
2024-11-15 12:24:25 +0800

    chore: added javadoc for ReplenishmentRequestStatus

--- src/enums/ReplenishmentRequestStatus.java
+++ src/enums/ReplenishmentRequestStatus.java
@@ -1,7 +1,19 @@
 package enums;
 
+/**
+ * Enum representing the status of a replenishment request.
+ * Used to indicate the current state of a request for inventory replenishment.
+ * 
+ * @author Lim Jun Howe 
+ * @version 1.0 
+ */
 public enum ReplenishmentRequestStatus {
+    /** The replenishment request is pending and awaiting approval. */
     PENDING,
+    
+    /** The replenishment request has been reviewed and rejected. */
     REJECTED,
+    
+    /** The replenishment request has been reviewed and approved. */
     APPROVED
 }
