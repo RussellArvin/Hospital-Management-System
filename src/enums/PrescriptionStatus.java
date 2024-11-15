50e42b74decaed18361530accb5e055f7f2101ad
mommypokko <c3l3st7@gmail.com>
2024-11-15 12:23:22 +0800

    chore: added javadoc for PrescriptionsStatus

--- src/enums/PrescriptionStatus.java
+++ src/enums/PrescriptionStatus.java
@@ -1,6 +1,16 @@
 package enums;
 
+/**
+ * Enum representing the status of a prescription.
+ * Used to track the progress of prescription processing.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0 
+ */
 public enum PrescriptionStatus {
+    /** Prescription is pending and has not been dispensed yet. */
     PENDING,
+    
+    /** Prescription has been dispensed to the patient. */
     DISPENSED
 }
