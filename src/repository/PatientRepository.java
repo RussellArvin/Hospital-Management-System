071ce2c3af498a3b9092458a953fe12091e00eab
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:50:29 +0800

    chore: added javadoc for PatientRepository

--- src/repository/PatientRepository.java
+++ src/repository/PatientRepository.java
@@ -1,25 +1,48 @@
 package repository;
 
-
 import model.Patient;
 import repository.base.CsvRepository;
 import repository.mapper.PatientMapper;
 
-public class PatientRepository extends CsvRepository<Patient,PatientMapper> {
+/**
+ * The PatientRepository class extends CsvRepository for managing Patient entities.
+ * It specifies the CSV file and header used for Patient data and provides methods
+ * for accessing and managing Patient records based on their name.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0
+ */
+public class PatientRepository extends CsvRepository<Patient, PatientMapper> {
+
     private static final String CSV_FILE = "data/patients.csv";
     private static final String CSV_HEADER = "id,password,salt,name,age,dateOfBirth,gender,bloodType,phoneNumber,email,createdAt,updatedAt";
 
+    /**
+     * Constructs a PatientRepository with predefined CSV file and header information
+     * for storing Patient data and initializes the PatientMapper.
+     */
     public PatientRepository() {
         super(CSV_FILE, CSV_HEADER, new PatientMapper());
     }
 
+    /**
+     * Retrieves all Patient records from the CSV file.
+     *
+     * @return An array of all Patient objects in the repository.
+     */
     @Override
     public Patient[] findAll() {
         return super.findAll(Patient.class);
     }
 
-    public Patient findOneByName(String patientName){
-        String line = this.fileManager.findLineByColumnValue("name",patientName);
+    /**
+     * Finds and returns a Patient record by the specified name.
+     *
+     * @param patientName The name of the patient to search for.
+     * @return The Patient object with the specified name, or null if not found.
+     */
+    public Patient findOneByName(String patientName) {
+        String line = this.fileManager.findLineByColumnValue("name", patientName);
         return this.mapper.fromCsvString(line);
     }
 }
