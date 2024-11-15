bacbf62813cb8dd630ee1085da072d4d47b4528e
mommypokko <c3l3st7@gmail.com>
2024-11-15 13:14:48 +0800

    chore: added javadoc for ReplenishmentRequest

--- src/model/ReplenishmentRequest.java
+++ src/model/ReplenishmentRequest.java
@@ -1,15 +1,33 @@
 package model;
 
+import enums.ReplenishmentRequestStatus;
 import java.time.LocalDateTime;
 
-import enums.ReplenishmentRequestStatus;
-
+/**
+ * The ReplenishmentRequest class represents a request to replenish the stock of a specific medicine.
+ * It includes details such as the medicine, the pharmacist making the request, the requested amount, and the request status.
+ * 
+ * @author Tan Jou Yuan 
+ * @version 1.0 
+ */
 public class ReplenishmentRequest extends BaseEntity {
+
     private String medicineId;
     private String pharmacistId;
     private int newAmount;
     ReplenishmentRequestStatus status;
 
+    /**
+     * Constructs a ReplenishmentRequest with the specified details, including ID and timestamps.
+     *
+     * @param id            The unique ID of the replenishment request.
+     * @param medicineId    The ID of the medicine to be replenished.
+     * @param pharmacistId  The ID of the pharmacist making the request.
+     * @param newAmount     The amount of medicine requested for replenishment.
+     * @param status        The current status of the replenishment request.
+     * @param createdAt     The date and time when the request was created.
+     * @param updatedAt     The date and time when the request was last updated.
+     */
     public ReplenishmentRequest(
         String id,
         String medicineId,
@@ -26,11 +44,18 @@ public class ReplenishmentRequest extends BaseEntity {
         this.status = status;
     }
 
+    /**
+     * Constructs a ReplenishmentRequest with pending status and without specifying ID and timestamps.
+     *
+     * @param medicineId    The ID of the medicine to be replenished.
+     * @param pharmacistId  The ID of the pharmacist making the request.
+     * @param newAmount     The amount of medicine requested for replenishment.
+     */
     public ReplenishmentRequest(
         String medicineId,
         String pharmacistId,
         int newAmount
-    ){
+    ) {
         super();
         this.medicineId = medicineId;
         this.pharmacistId = pharmacistId;
@@ -38,31 +63,66 @@ public class ReplenishmentRequest extends BaseEntity {
         this.status = ReplenishmentRequestStatus.PENDING;
     }
 
-    public int getNewAmount(){
+    /**
+     * Gets the amount of medicine requested for replenishment.
+     *
+     * @return The requested amount.
+     */
+    public int getNewAmount() {
         return this.newAmount;
     }
 
-    public String getMedicineId(){
+    /**
+     * Gets the ID of the medicine to be replenished.
+     *
+     * @return The medicine ID.
+     */
+    public String getMedicineId() {
         return this.medicineId;
     }
 
-    public String getPharmacistId(){
+    /**
+     * Gets the ID of the pharmacist making the replenishment request.
+     *
+     * @return The pharmacist ID.
+     */
+    public String getPharmacistId() {
         return this.pharmacistId;
     }
 
-    public ReplenishmentRequestStatus getStatus(){
+    /**
+     * Gets the current status of the replenishment request.
+     *
+     * @return The status of the request.
+     */
+    public ReplenishmentRequestStatus getStatus() {
         return this.status;
     }
     
+    /**
+     * Sets the new amount of medicine requested for replenishment.
+     *
+     * @param newAmount The new amount to set.
+     */
     public void setNewAmount(int newAmount) {
         this.newAmount = newAmount;
     }
 
-    public void setStatus(ReplenishmentRequestStatus status){
+    /**
+     * Sets the status of the replenishment request.
+     *
+     * @param status The status to set for the request.
+     */
+    public void setStatus(ReplenishmentRequestStatus status) {
         this.status = status;
     }
 
-    public String toCsvString(){
+    /**
+     * Returns a CSV string representation of the replenishment request, including all relevant attributes.
+     *
+     * @return A CSV-formatted string containing replenishment request data.
+     */
+    public String toCsvString() {
         return String.join(",",
             id,
             medicineId,
