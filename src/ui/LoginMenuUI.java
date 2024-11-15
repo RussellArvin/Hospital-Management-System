874921421de9b24766a6b4a6cb5ee62cb2383187
mommypokko <c3l3st7@gmail.com>
2024-11-15 20:00:21 +0800

    chore: added javadoc for LoginMenuUI

--- src/ui/LoginMenuUI.java
+++ src/ui/LoginMenuUI.java
@@ -1,7 +1,19 @@
 package ui;
 
+/**
+ * The LoginMenuUI class provides a user interface for the login menu of the Hospital Management System.
+ * It extends the MenuUI class and displays the available login and registration options for users.
+ * 
+ * @author Natalyn Pong 
+ * @version 1.0
+ */
 public class LoginMenuUI extends MenuUI {
-    public void printOptions(){
+
+    /**
+     * Prints the options available in the login menu.
+     * Users can choose to login as a patient or staff, register as a patient, or exit the system.
+     */
+    public void printOptions() {
         printMenu("Hospital Management System",
             "1. Login as Patient",
             "2. Register as Patient",
