5fec4e3e92e4c75e4270c7d4bd2afd641444367e
mommypokko <c3l3st7@gmail.com>
2024-11-15 12:20:45 +0800

    chore: added javadoc for Gender

--- src/enums/Gender.java
+++ src/enums/Gender.java
@@ -1,6 +1,16 @@
 package enums;
 
+/**
+ * Enum representing the gender of a person.
+ * Used for categorization and identification purposes.
+ * 
+ * @author Russel Arvin 
+ * @version 1.0 
+ */
 public enum Gender {
+    /** Male gender. */
     MALE,
+    
+    /** Female gender. */
     FEMALE
 }
