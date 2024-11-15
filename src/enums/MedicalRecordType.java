f1080f2cbc9ad7e67ddeefecc94e0056481211ca
mommypokko <c3l3st7@gmail.com>
2024-11-15 12:22:31 +0800

    chore: added javadoc for MedicalRecordType

--- src/enums/MedicalRecordType.java
+++ src/enums/MedicalRecordType.java
@@ -1,6 +1,16 @@
 package enums;
 
+/**
+ * Enum representing the types of medical records.
+ * This is used to categorize the type of medical record entry.
+ * 
+ * @author Russell Arvin 
+ * @version 1.0
+ */
 public enum MedicalRecordType {
+    /** A medical record entry for diagnosis information. */
     DIAGNOSIS,
+    
+    /** A medical record entry for treatment information. */
     TREATMENT
 }
